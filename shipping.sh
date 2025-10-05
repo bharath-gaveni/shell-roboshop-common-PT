b475ae5e6913cb3afdc6d50d505752e7f578f3fa
bharath-gaveni <bharathgaveni@gmail.com>
2025-10-06 00:42:53 +0530

    script exe

--- shipping.sh
+++ shipping.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source ./common.sh
 name=shipping
-Dir_name=$PWD
 mysql_Host_name=mysql.bharathgaveni.fun
 check_root
 setup_logging
@@ -10,19 +9,15 @@ java_setup
 systemd_setup
 
 dnf install mysql -y &>>$log_file
-validate $? "installing mysql client software to connect with mysql and run the DB scripts"
+validate $? "installing mysql client to load data to mysql DB"
 
 mysql -h $mysql_Host_name -uroot -pRoboShop@1 -e 'use cities' &>>$log_file
 if [ $? -ne 0 ]; then
     mysql -h $mysql_Host_name -uroot -pRoboShop@1 < /app/db/schema.sql &>>$log_file
-    mysql -h $mysql_Host_name -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$log_file
+    mysql -h $mysql_Host_name -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$log_file
     mysql -h $mysql_Host_name -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$log_file
-    echo -e "$G data is loaded succesfully $N" | tee -a $log_file
 else
-    echo -e "Data is already is loaded $Y SKIPPING $N" | tee -a $log_file
-fi
-
-systemctl restart $name &>>$log_file
-validate $? "restarted the $name"
-
+    echo "Shipping data is already loaded"
+fi  
+restart_app
 print_time
