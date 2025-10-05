46ee4eacd1b3407207c8c25e409baeda0587e12b
bharath-gaveni <bharathgaveni@gmail.com>
2025-10-05 21:43:30 +0530

    script exe

--- redis.sh
+++ redis.sh
@@ -4,22 +4,22 @@ source ./common.sh
 check_root
 setup_logging
 
-dnf module disable redis -y
+dnf module disable redis -y &>>$log_file
 validate $? "disabling redis"
 
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$log_file
 validate $? "enabling redis"
 
-dnf install redis -y
+dnf install redis -y &>>$log_file
 validate $? "installing redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' -e 'protected-mode/ c protected-mode no ' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' -e 'protected-mode/ c protected-mode no ' /etc/redis/redis.conf &>>$log_file
 validate $? "Allowing remote connections to redis and updated protected mode from yes to no"
 
-systemctl enable redis
+systemctl enable redis &>>$log_file
 validate $? "eanbling redis"
 
-systemctl start redis
+systemctl start redis &>>$log_file
 validate $? "start redis"
 
 print_time
