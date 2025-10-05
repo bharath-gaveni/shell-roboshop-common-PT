ff7a2b44559e540556b2da22a3a0ec35fa0314be
bharath-gaveni <bharathgaveni@gmail.com>
2025-10-06 00:17:49 +0530

    script exe

--- shipping.sh
+++ shipping.sh
@@ -15,8 +15,8 @@ validate $? "installing mysql client software to connect with mysql and run the 
 mysql -h $mysql_Host_name -uroot -pRoboShop@1 -e 'use cities' &>>$log_file
 if [ $? -ne 0 ]; then
     mysql -h $mysql_Host_name -uroot -pRoboShop@1 < /app/db/schema.sql &>>$log_file
-    mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$log_file
-    mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$log_file
+    mysql -h $mysql_Host_name -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$log_file
+    mysql -h $mysql_Host_name -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$log_file
     echo -e "$G data is loaded succesfully $N" | tee -a $log_file
 else
     echo "Data is already is loaded $Y SKIPPING $N" | tee -a $log_file
